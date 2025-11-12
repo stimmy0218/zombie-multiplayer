f72b14ef8aa98cea267f6621d317b4eddbca41ee
stimmy0218 <stimmy0218@gmail.com>
2025-11-12 16:38:14 +0900

    움직임 구현

--- Assets/Scripts/Woman.cs
+++ Assets/Scripts/Woman.cs
@@ -1,18 +1,83 @@
+using System.Collections;
 using Photon.Pun;
 using UnityEngine;
 
 public class Woman : MonoBehaviourPun
 {
+    public float speed = 5f;   
+    private Rigidbody rb;
+    Animator animator;
+    private float jumpForce = 5f;
+    private bool jump;
+    private float jumpCooldown = 0.25f;
+    private bool canJump = true;
     
     
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
-
     
     void Update()
     {
+        if(!photonView.IsMine)
+            return;
         
+        Move();
+        Jump();
     }
+
+    void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(h, 0f, v);
+        
+        if (direction != Vector3.zero)
+        {
+            var q = Quaternion.LookRotation(direction);
+            transform.rotation = q;
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            animator.SetInteger("State", 1);
+        }
+        else
+        {
+            animator.SetInteger("State", 0);
+        }
+    }
+    
+    //점프 메서드
+    void Jump()
+    {
+        jump = Input.GetKeyDown(KeyCode.Space);
+
+        if (canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            //animator.SetInteger("Jump", 1);
+            Debug.Log("점프!");
+            canJump = false;
+            StartCoroutine(JumpCooldown());
+        }
+    }
+    
+    //점프 쿨타임
+    private IEnumerator JumpCooldown()
+    {
+        yield return new WaitForSeconds(jumpCooldown);
+        //animator.SetFloat("Move", vertical);
+    }
+    
+    //착지 
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;
+            //animator.SetInteger("Jump", 0);
+            //animator.SetFloat("Move", vertical);
+        }
+    }
+    
 }
