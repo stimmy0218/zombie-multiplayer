0ebb3850bd70457ede3d48e12d257690c3362d1f
stimmy0218 <stimmy0218@gmail.com>
2025-11-12 14:47:50 +0900

    버튼 생성

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,73 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    private string gameVersion = "1";
+    public Button btn;
+    
+    
+    void Start()
+    {
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+        
+        btn.interactable = false;
+        btn.onClick.AddListener(() => 
+        { 
+            Debug.Log("룸 접속 요청");
+            btn.interactable = false;
+            Connect();
+        });
+    }
+
+    void Connect()
+    {
+        Debug.Log($"IsConnected: {PhotonNetwork.IsConnected}");
+        
+        if(PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnected");
+        btn.interactable = true;
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("OnDisconnected");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log($"OnJoinRandomFailed: {returnCode}, {message}");
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 2 });
+    }
+    
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("OnCreatedRoom");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log($"OnCreateRoomFailed: {returnCode}, {message}");
+    }
+}
+
